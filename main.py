20c847ef042342575e333a76c69f1a66abf36b23
Iskandernow <49417047+Iskandernow@users.noreply.github.com>
2024-07-20 01:46:44 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,8 @@
+print("1st program")
+print(9 ** 0.5 * 5)
+print("2nd program")
+print(9.99 > 9.98 and 1000 != 1000.1 )
+print("3st program")
+print(1234 // 10 % 100 + 5678 // 10 % 100)
+print("4st program")
+print(int(13.42) == (42.13 * 100 % 100) or int(13.42 * 100 % 100) == int(42.13))
