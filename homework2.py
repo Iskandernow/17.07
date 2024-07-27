9fa57a47464d777135ebdff2c28561dddda6827c
Iskandernow <49417047+Iskandernow@users.noreply.github.com>
2024-07-27 13:16:53 +0300

    Add files via upload

---
+++ homework2.py
@@ -0,0 +1,5 @@
+number_of_homework_assignments_completed = 12
+number_of_hours_spent = 1.5
+course_name = "Python"
+time_for_one_task = 0.125
+print('Курс:', course_name, 'всего задач:', number_of_homework_assignments_completed, 'затрачено часов:', number_of_hours_spent, 'среднее время выполнения', time_for_one_task)
