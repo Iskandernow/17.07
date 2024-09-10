550c0546578f619d0c43d575941a4365f346e6a9
Iskandernow <49417047+Iskandernow@users.noreply.github.com>
2024-09-10 18:13:19 +0300

    module_1_6.py

--- module_1_6.py
+++ module_1_6.py
@@ -4,13 +4,11 @@ print(my_dict['Alex'])
 my_dict['Anton'] = 1990
 my_dict.update({'Vitaly': 1962,
                 'Anna': 1960})
-del my_dict['Olga']
-print(my_dict.get('Olga'))
-print(my_dict)
+a = my_dict.pop('Olga')
+print(a)
 my_set = {1, 2, 7, 9, 2, 9, 'button', True, (18, 5, 6)}
 print(my_set)
-print(my_set.add(3))
-print(my_set.add(63))
-print(my_set)
+my_set.add(3)
+my_set.add(63)
 print(my_set.remove(2))
 print(my_set)
